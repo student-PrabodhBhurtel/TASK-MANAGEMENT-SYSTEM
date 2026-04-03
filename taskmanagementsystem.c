493f27caf84530774e5c797e6d7872025abc8a13
student-PrabodhBhurtel <prabodhbhurtel0@gmail.com>
2026-04-03 16:05:49 +0545

    Update taskmanagementsystem.c

--- taskmanagementsystem.c
+++ taskmanagementsystem.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct task{
-int id;
-chat name[50];
-char deadline[50];
-char status[50];
-}t;
+struct task {
+    int id;
+    char name[50];
+    char deadline[50];
+    char status[50];
+} t;
 
 void addTask();
 void displayTask();
@@ -15,10 +15,12 @@ void searchTask();
 void updateTask();
 void deleteTask();
 
-int main(){
- char ch;
- printf("\n=====================================\n");
-        printf("      TASK MANAGEMENT SYSTEM\n");
+int main() {
+    int choice;
+
+    do {
+        printf("\n=====================================\n");
+        printf("        TASK MANAGEMENT SYSTEM\n");
         printf("=====================================\n");
         printf("1. Add Task\n");
         printf("2. Display Tasks\n");
@@ -26,31 +28,32 @@ int main(){
         printf("4. Update Task\n");
         printf("5. Delete Task\n");
         printf("6. Exit\n");
- printf("Enter your choice:");
- scanf("%d",&choice);
+        printf("Enter your choice: ");
+        scanf("%d",&choice);
 
- switch(choice){
-  case 1:
-  addTask();
-  break;
-   case 2:
-  displayTask();
-  break;
-   case 3:
-  searchTask();
-  break;
-   case 4:
-  updateTask();
-  break;
-   case 5:
-  deleteTask();
-  break;
-     case 6:
-  printf("Exiting Program...\n");
-  break;
-   Default:
-  printf("Invalid Choice!!! Please try again.\n");  
- }
- return 0;
+        switch(choice) {
+            case 1:
+                addTask();
+                break;
+            case 2:
+                displayTask();
+                break;
+            case 3:
+                searchTask();
+                break;
+            case 4:
+                updateTask();
+                break;
+            case 5:
+                deleteTask();
+                break;
+            case 6:
+                printf("Exiting Program...\n");
+                break;
+            default:
+                printf("Invalid Choice!!! Please try again.\n");  
+        }
+    } while(choice != 6);
+
+    return 0;
 }
-

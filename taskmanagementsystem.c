02cbee2fcec340dec3ea60a3c0856aeb34fedb38
student-PrabodhBhurtel <prabodhbhurtel0@gmail.com>
2026-04-03 15:55:05 +0545

    Update taskmanagementsystem.c

--- taskmanagementsystem.c
+++ taskmanagementsystem.c
@@ -16,6 +16,7 @@ void updateTask();
 void deleteTask();
 
 int main(){
+ char ch;
  printf("\n=====================================\n");
         printf("      TASK MANAGEMENT SYSTEM\n");
         printf("=====================================\n");
@@ -25,5 +26,31 @@ int main(){
         printf("4. Update Task\n");
         printf("5. Delete Task\n");
         printf("6. Exit\n");
+ printf("Enter your choice:");
+ scanf("%d",&choice);
+
+ switch(choice){
+  case 1:
+  addTask();
+  break;
+   case 2:
+  displayTask();
+  break;
+   case 3:
+  searchTask();
+  break;
+   case 4:
+  updateTask();
+  break;
+   case 5:
+  deleteTask();
+  break;
+     case 6:
+  printf("Exiting Program...\n");
+  break;
+   Default:
+  printf("Invalid Choice!!! Please try again.\n");  
+ }
+ return 0;
 }
 
